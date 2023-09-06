dc3974d4f4f09bdd306854c43707287f35a987f1
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2023-09-06 08:49:00 -0400

    slight reformatting

--- src/typeParsers/formatter.py
+++ src/typeParsers/formatter.py
@@ -1,3 +1,62 @@
+"""
+Handles the formatting of tossups and bonuses into easily usable tossup/bonus objects.
+
+Author: Will Hoover
+"""
+
+def is_start_of_question(line: str) -> bool:
+    """
+    Returns true if the given line is the start of a question, false otherwise.
+    """
+    try:
+        int(line[0])
+        if line[1] == '.' or line[2] == '.':
+            return True
+        else:
+            return False
+    except:
+        return False
+    
+def remove_bad_stuff(line: str) -> str:
+    """
+    Removes the following from tossup or bonus lines
+    - moderator notes
+    - numbering
+    - "ANSWER:"
+    - pronunciation guides
+    - author notes
+    """
+    if len(line) == 0:
+        return ""
+    try:
+        if line[:14].lower() == "moderator note" or line[:17].lower() == "note to moderator":
+            return ""
+    except:
+        pass
+    if is_start_of_question(line):
+        line = line[3:].strip()
+        return line + '\n'
+    if line[:3] == "[10":
+        return line[line.find("]"):] + '\n'
+    try:
+        if(line[0:7].lower() == "answer:"):
+            return line[7:].strip() + '\n'
+    except:
+        pass
+    if '(“' in line:
+        start = line.find('(“')
+        if '”)' in line:
+            end = line.find('”)') + 2
+            return line[:start-1] + line[end:] + '\n'
+        else:
+            return line[:start] + '\n'
+    elif '”)' in line:
+            end = line.find('”)') + 2
+            return line[end:] + '\n'
+    if line[0] == "<" and line[-1] == ">":
+        return ""
+    return line + '\n'
+
 def format_tossups(tossups: str) -> list:
     """
     Given a string containing all tossup lines in a packet, returns
@@ -11,7 +70,7 @@ def format_tossups(tossups: str) -> list:
     is_answer_line = False
     for line in tossup_lines:
         if is_answer_line:
-            if starts_with_number(line):
+            if is_start_of_question(line):
                 if len(current_tossup) != 0:
                     tossup_list.append(current_tossup)
                     current_tossup = dict()
@@ -54,7 +113,7 @@ def format_bonuses(bonuses: str) -> list:
         elif line[:7].lower() == "answer:":
             current_field = "answer-" + str(len(current_bonus) // 2)
             current_bonus[current_field] = remove_bad_stuff(line)
-        elif starts_with_number(line):
+        elif is_start_of_question(line):
             if len(current_bonus) > 0:
                 bonus_list.append(current_bonus)
             current_bonus = dict()
@@ -65,57 +124,3 @@ def format_bonuses(bonuses: str) -> list:
     if len(current_bonus) > 0:
         bonus_list.append(current_bonus)
     return bonus_list
-            
-
-def remove_bad_stuff(line: str) -> str:
-    """
-    Removes the following from tossup or bonus lines
-    - moderator notes
-    - numbering
-    - "ANSWER:"
-    - pronunciation guides
-    - author notes
-    """
-    if len(line) == 0:
-        return ""
-    try:
-        if line[:14].lower() == "moderator note" or line[:17].lower() == "note to moderator":
-            return ""
-    except:
-        pass
-    if starts_with_number(line):
-        line = line[3:].strip()
-        return line + '\n'
-    if line[:3] == "[10":
-        return line[line.find("]"):] + '\n'
-    try:
-        if(line[0:7].lower() == "answer:"):
-            return line[7:].strip() + '\n'
-    except:
-        pass
-    if '(“' in line:
-        start = line.find('(“')
-        if '”)' in line:
-            end = line.find('”)') + 2
-            return line[:start-1] + line[end:] + '\n'
-        else:
-            return line[:start] + '\n'
-    elif '”)' in line:
-            end = line.find('”)') + 2
-            return line[end:] + '\n'
-    if line[0] == "<" and line[-1] == ">":
-        return ""
-    return line + '\n'
-
-def starts_with_number(line: str) -> bool:
-    """
-    Returns true if the given line starts with a number, false otherwise.
-    """
-    try:
-        int(line[0])
-        if line[1] == '.' or line[2] == '.':
-            return True
-        else:
-            return False
-    except:
-        return False
