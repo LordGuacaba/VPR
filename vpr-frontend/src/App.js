51d251ede585dd3b089f1bb98dab9204132f8136
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2024-06-15 10:25:39 -0400

    initial frontend setup

---
+++ vpr-frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Button, Container } from "@mui/material"
+
+
+function App() {
+  return (
+    <Container>
+      <h1>VPR</h1>
+      <input type="file"></input>
+      <Button variant="contained">Submit</Button>
+    </Container>
+    
+  );
+}
+
+export default App;
